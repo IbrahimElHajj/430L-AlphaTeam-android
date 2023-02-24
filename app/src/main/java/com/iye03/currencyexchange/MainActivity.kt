e6274a9948fa7934b89ab94435848986613da959
IbrahimElHajj <iye03@mail.aub.edu>
2023-02-24 14:26:29 +0200

    phase 2 done

--- app/src/main/java/com/iye03/currencyexchange/MainActivity.kt
+++ app/src/main/java/com/iye03/currencyexchange/MainActivity.kt
@@ -2,10 +2,36 @@ package com.iye03.currencyexchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.iye03.currencyexchange.api.ExchangeService
+import com.iye03.currencyexchange.api.model.ExchangeRates
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
+    private var buyUsdTextView: TextView? = null
+    private var sellUsdTextView: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buyUsdTextView = findViewById(R.id.txtBuyUsdRate)
+        sellUsdTextView = findViewById(R.id.txtSellUsdRate)
+
+        ExchangeService.exchangeApi().getExchangeRates().enqueue(object :
+            Callback<ExchangeRates> {
+            override fun onResponse(call: Call<ExchangeRates>, response:
+            Response<ExchangeRates>) {
+                val responseBody: ExchangeRates? = response.body();
+                buyUsdTextView?.text = responseBody?.lbpToUsd.toString()
+                sellUsdTextView?.text = responseBody?.usdToLbp.toString()
+            }
+            override fun onFailure(call: Call<ExchangeRates>, t: Throwable) {
+                return;
+            }
+        })
+
+
     }
 }
---
+++ app/src/main/java/com/iye03/currencyexchange/api/retrofit.kt
@@ -0,0 +1,24 @@
+package com.iye03.currencyexchange.api
+
+import com.iye03.currencyexchange.api.model.ExchangeRates
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+
+object ExchangeService {
+    private const val API_URL: String = "http://192.168.0.106:5000"
+    fun exchangeApi():Exchange {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        // Create an instance of our Exchange API interface.
+        return retrofit.create(Exchange::class.java);
+    }
+    interface Exchange {
+        @GET("/exchangeRate")
+        fun getExchangeRates(): Call<ExchangeRates>
+    }
+}
