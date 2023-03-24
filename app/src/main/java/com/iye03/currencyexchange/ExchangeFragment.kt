250682725dbe4e902f2c1f5297ec750672a3bb36
Ibrahim <iye03@mail.aub.edu>
2023-03-24 12:13:54 +0200

    done phase 2

--- app/src/main/java/com/iye03/currencyexchange/ExchangeFragment.kt
+++ app/src/main/java/com/iye03/currencyexchange/ExchangeFragment.kt
@@ -1,20 +1,33 @@
 package com.iye03.currencyexchange
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import com.iye03.currencyexchange.api.ExchangeService
 import com.iye03.currencyexchange.api.model.ExchangeRates
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 class ExchangeFragment : Fragment() {
     private var buyUsdTextView: TextView? = null
     private var sellUsdTextView: TextView? = null
+
+    private var lbpBuyNum: EditText? = null
+    private var usdBuyNum: EditText? = null
+    private var lbpSellNum: EditText? = null
+    private var usdSellNum: EditText? = null
+    private var lock: Boolean = false
+
 
     private fun fetchRates(){
         ExchangeService.exchangeApi().getExchangeRates().enqueue(object :
@@ -45,6 +58,9 @@ class ExchangeFragment : Fragment() {
         fetchRates()
     }
 
+    fun listener(view: TextView, i:Int, k: KeyEvent): Boolean {
+        return false
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -54,6 +70,103 @@ class ExchangeFragment : Fragment() {
 
         buyUsdTextView = view.findViewById(R.id.txtBuyUsdRate)
         sellUsdTextView = view.findViewById(R.id.txtSellUsdRate)
+
+        lbpBuyNum = view.findViewById(R.id.lbpbuy)
+        usdBuyNum = view.findViewById(R.id.usdbuy)
+        lbpSellNum = view.findViewById(R.id.lbpsell)
+        usdSellNum = view.findViewById(R.id.usdsell)
+
+        lbpBuyNum?.addTextChangedListener(object:TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!lock) {
+                    lock = true
+                    var lbp = try {lbpBuyNum?.text.toString().toDouble()} catch (e: NumberFormatException){0.0}
+                    var rate = buyUsdTextView?.text.toString().toDouble()
+                    usdBuyNum?.setText(
+                        ((lbp / rate * 100).roundToInt().toDouble() / 100).toString()
+                    )
+                }else{
+                    lock = false
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
+        usdBuyNum?.addTextChangedListener(object:TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!lock) {
+                    lock = true
+                    var usd = try{usdBuyNum?.text.toString().toDouble()} catch (e: NumberFormatException){0.0}
+                    var rate = buyUsdTextView?.text.toString().toDouble()
+                    lbpBuyNum?.setText(
+                        ((usd * rate).roundToLong()).toString()
+                    )
+
+                }else{
+                    lock = false
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
+
+        lbpSellNum?.addTextChangedListener(object:TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!lock) {
+                    lock = true
+                    var lbp = try {lbpSellNum?.text.toString().toDouble()} catch (e: NumberFormatException){0.0}
+                    var rate = sellUsdTextView?.text.toString().toDouble()
+                    usdSellNum?.setText(
+                        ((lbp / rate * 100).roundToInt().toDouble() / 100).toString()
+                    )
+                }else{
+                    lock = false
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
+        usdSellNum?.addTextChangedListener(object:TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!lock) {
+                    lock = true
+                    var usd = try{usdSellNum?.text.toString().toDouble()} catch (e: NumberFormatException){0.0}
+                    var rate = sellUsdTextView?.text.toString().toDouble()
+                    lbpSellNum?.setText(
+                        ((usd * rate).roundToLong()).toString()
+                    )
+
+                }else{
+                    lock = false
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
 
         return view
     }
